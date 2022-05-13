2eacfef5f4c18da8398fd999fea1bdea46f52528
Wanja Pernath <wpernath@redhat.com>
2022-05-13 14:45:35 +0200

    first commit

---
+++ src/main/java/org/wanja/fatcat/Maze.java
@@ -0,0 +1,61 @@
+package org.wanja.fatcat;
+
+public class Maze {
+  public String name;
+  public String description;
+  public String author;
+  public String tileSet;
+  public int width;
+  public int height;
+
+  public int[][] mazeData;
+  public boolean[][] logicData;
+
+  public int mouseX = 0;
+  public int mouseY = 0;
+  public int catX = 0;
+  public int catY = 0;
+
+  public Maze(String name, String author, int width, int height) {
+    this.name = name;
+    this.author = author;
+    this.width = width;
+    this.height = height;
+  }
+
+  public void parseMap(String mapData) {
+    String[] rows = mapData.split("\\n");
+    int h = rows.length;
+    if( h != height) throw new IllegalArgumentException();
+
+    mazeData  = new int[height][width];
+    logicData = new boolean[height][width];
+
+    for( int y = 0; y<h; y++) {
+      String row = rows[y];
+      if( row.length() != width ) throw new IllegalArgumentException();
+
+      for( int x = 0; x < width; x++) {
+        char c = row.charAt(x);
+
+        if( c == ' ') mazeData[y][x] = 0;
+        else if ( c== '#') mazeData[y][x] = 10;
+        else if ( c== 'm') {
+          c = ' ';
+          mouseX = x;
+          mouseY = y;
+          mazeData[y][x] = 0;
+        }
+        else if (c == 'c') {
+          c = ' ';
+          catX = x;
+          catY = y;
+          mazeData[y][x] = 0;
+        }
+
+        logicData[y][x] = c != ' ';
+      }
+    }
+  }
+  
+}
---
+++ src/main/java/org/wanja/fatcat/MazeResource.java
@@ -0,0 +1,52 @@
+package org.wanja.fatcat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+@Path("/maze")
+public class MazeResource {
+  
+  List<Maze> mazes = new ArrayList<Maze>();
+  
+
+  public MazeResource() {
+    Maze m = new Maze("Standard", "Wanja", 31, 23);
+    mazes.add(m);
+    m.parseMap(
+        """
+            ###############################
+            #              m              #
+            # ############# ############# #
+            # #   #   #   # #   #   #   # #
+            # # # # # # # # # # # # # # # #
+            # # # # # # # # # # # # # # # #
+            # # # # # # # # # # # # # # # #
+            # # #   # # #     # # #   # # #
+            # # ##### # ####### # ##### # #
+            # #     # #         # #     # #
+            # ##### # ##### ##### # ##### #
+            #       #             #       #
+            # ##### # ##### ##### # ##### #
+            # #     # #         # #     # #
+            # # ##### # ####### # ##### # #
+            # # #   # # #  c  # # #   # # #
+            # # # # # # # # # # # # # # # #
+            # # # # # # # # # # # # # # # #
+            # # # # # # # # # # # # # # # #
+            # #   #   #   # #   #   #   # #
+            # ############# ############# #
+            #                             #
+            ###############################"""      
+    );
+  }
+
+  @GET
+  @Path("{id}")
+  public Maze mazeById( @PathParam Long id) {
+    return mazes.get(0);
+  }
+}
