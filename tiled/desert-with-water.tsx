a9aae7f60fdf847211643dc4cc81ea4361407c69
Wanja Pernath <wpernath@redhat.com>
2022-05-17 21:17:03 +0200

    committing some tiled stuff

---
+++ tiled/desert-with-water.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.90" name="desert-with-water" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="66" columns="11">
+ <image source="../src/main/resources/META-INF/resources/images/tilesets/desert-water.png" width="364" height="199"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="2" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="11" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="12" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="13" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="22" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="23" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="24" wangid="0,1,0,1,0,1,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
