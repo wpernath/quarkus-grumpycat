534546250f3fc59829a09892be6fdeef42dee04b
wpernath <w.pernath@gmx.de>
2022-05-18 09:54:48 +0200

    first level via tiled mapeditor

--- tiled/desert-with-water.tsx
+++ tiled/desert-with-water.tsx
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.90" name="desert-with-water" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="66" columns="11">
+<tileset version="1.8" tiledversion="1.8.5" name="desert-with-water" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="66" columns="11">
  <image source="../src/main/resources/META-INF/resources/images/tilesets/desert-water.png" width="364" height="199"/>
  <wangsets>
-  <wangset name="Unnamed Set" type="corner" tile="-1">
-   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  <wangset name="Desert" type="corner" tile="-1">
+   <wangcolor name="Stone" color="#ff0000" tile="-1" probability="1"/>
    <wangtile tileid="0" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="1" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="2" wangid="0,1,0,1,0,1,0,1"/>
