2b250e96ff2d7226b460e503d66b92ea2b2d6068
Wanja Pernath <wpernath@redhat.com>
2022-06-12 09:22:41 +0200

    changed app props

--- tiled/spiders.tsx
+++ tiled/spiders.tsx
@@ -1,4 +1,55 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.5" name="spiders" tilewidth="64" tileheight="64" tilecount="50" columns="10">
  <image source="../../../Downloads/LPC_Spiders/spider01.png" width="640" height="320"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="250"/>
+   <frame tileid="4" duration="250"/>
+   <frame tileid="5" duration="250"/>
+   <frame tileid="6" duration="250"/>
+   <frame tileid="7" duration="250"/>
+   <frame tileid="8" duration="250"/>
+   <frame tileid="9" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="250"/>
+   <frame tileid="14" duration="250"/>
+   <frame tileid="15" duration="250"/>
+   <frame tileid="16" duration="250"/>
+   <frame tileid="17" duration="250"/>
+   <frame tileid="18" duration="250"/>
+   <frame tileid="19" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <animation>
+   <frame tileid="20" duration="250"/>
+   <frame tileid="24" duration="250"/>
+   <frame tileid="25" duration="250"/>
+   <frame tileid="26" duration="250"/>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="28" duration="250"/>
+   <frame tileid="29" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="30">
+  <animation>
+   <frame tileid="30" duration="250"/>
+   <frame tileid="35" duration="250"/>
+   <frame tileid="36" duration="250"/>
+   <frame tileid="37" duration="250"/>
+   <frame tileid="38" duration="250"/>
+   <frame tileid="39" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="40">
+  <animation>
+   <frame tileid="40" duration="250"/>
+   <frame tileid="41" duration="250"/>
+   <frame tileid="42" duration="250"/>
+   <frame tileid="43" duration="250"/>
+  </animation>
+ </tile>
 </tileset>
