56b90c8f240fa248825e76ee7eeede25dd353e5b
Wanja Pernath <wpernath@redhat.com>
2022-07-04 12:41:22 +0200

    implemented highscore service

--- src/main/java/org/wanja/fatcat/model/Score.java
+++ src/main/java/org/wanja/fatcat/model/Score.java
@@ -3,14 +3,13 @@ package org.wanja.fatcat.model;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 @Entity
-public class Score extends PanacheEntity {
-    @OneToOne
-    public Player player;
+public class Score extends PanacheEntity {    
+    public Long playerId;
+    public Long gameId;
     public long score;
     public Date time = new Date();
 }
